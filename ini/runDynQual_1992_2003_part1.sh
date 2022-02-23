1d665b3f0eca5a07254e912785046ed4b102360a
erjones-UU <66476019+erjones-UU@users.noreply.github.com>
2022-02-23 18:57:06 +0100

    Delete ini directory

--- ini/runDynQual_1992_2003_part1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -t 120:00:00
-#SBATCH -p normal
-#SBATCH -N 1
-cd /home/ejones/
-python DynQualModel/parallel_pcrglobwb_runner.py historical_run/setup_05min_1992_2003_part1.ini
--- ini/runDynQual_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -t 3:00:00
-#SBATCH -p normal
-#SBATCH -N 1
-cd /home/ejones/
-python DynQualModel/deterministic_runner.py historical_run/setup_test.ini
