63aecb66adab57b052fd6c89855a02741268a2f4
erjones-UU <66476019+erjones-UU@users.noreply.github.com>
2022-02-24 14:52:42 +0100

    Add files via upload

--- ini/runDynQual.sh
+++ ini/runDynQual.sh
@@ -7,11 +7,11 @@
 cd /home/ejones/
 
 #Calculate loads
-python new_DynQualModel/deterministic_runner.py new_ini_v2/setup_05min_online_calcLoad.ini &
-python new_DynQualModel/deterministic_runner_offline.py new_ini/setup_05min_offline_calcLoad.ini &
+python DynQualModel/deterministic_runner.py ini/setup_05min_online_calcLoad.ini &
+python DynQualModel/deterministic_runner_offline.py ini/setup_05min_offline_calcLoad.ini &
 
 #Force loads
-python new_DynQualModel/deterministic_runner.py new_ini/setup_05min_online_forceLoad.ini &
-python new_DynQualModel/deterministic_runner_offline.py new_ini/setup_05min_offline_forceLoad.ini &
+python DynQualModel/deterministic_runner.py ini/setup_05min_online_forceLoad.ini &
+python DynQualModel/deterministic_runner_offline.py ini/setup_05min_offline_forceLoad.ini &
 
 wait
