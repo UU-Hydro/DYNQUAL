4029354599b1609245d4febc8de6f12e11b86289
erjones-UU <66476019+erjones-UU@users.noreply.github.com>
2022-03-21 14:06:19 +0100

    Add files via upload

--- ini/runDynQual.sh
+++ ini/runDynQual.sh
@@ -8,6 +8,6 @@ cd /home/ejones/
 
 #DynQual runs with W5E5 forcing
 python DynQualModel/deterministic_runner.py ini/DynQual_05min.ini &
-python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini &
+#python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini &
 
 wait
--- ini/runDynQual_Global.sh
+++ ini/runDynQual_Global.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 12:00:00
+#SBATCH -t 120:00:00
 #SBATCH -p thin
 #SBATCH -N 1
 #SBATCH -n 128
