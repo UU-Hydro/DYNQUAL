052dd95c8979f2c6f0bbfd3dde74516cbd643bf5
erjones-UU <66476019+erjones-UU@users.noreply.github.com>
2022-07-18 12:16:34 +0200

    Add files via upload

--- ini/runDynQual.sh
+++ ini/runDynQual.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -t 10:00
+#SBATCH -t 3:00:00
 #SBATCH -p thin
 #SBATCH -N 1
 #SBATCH -n 128
 
 cd /home/ejones/
 
-#Online DynQual runs with W5E5 forcing (calcLoads == True)
-#python DynQualModel/deterministic_runner.py ini/DynQual_05min.ini & #single landmask
-python DynQualModel/parallel_pcrglobwb_runner.py ini/DynQual_05min.ini & #parallel
+###Online DynQual runs with W5E5 forcing (calcLoads == True or False)
+python DynQualModel/deterministic_runner.py ini/DynQual_05min.ini & #single landmask
+#python DynQualModel/parallel_pcrglobwb_runner.py ini/DynQual_05min.ini & #parallel
 
-#Offline DynQual runs with W5E5 forcing (calcLoads == False)
-#python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini & #single landmask
-#python DynQualModel/parallel_pcrglobwb_runner_offline.py ini/DynQual_05min_offline.ini & #single landmask
+###Offline DynQual runs with W5E5 forcing (calcLoads == False only.)
+python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini & #single landmask
+#python DynQualModel/parallel_pcrglobwb_runner_offline.py ini/DynQual_05min_offline.ini & #parallel
 
 wait
