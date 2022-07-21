f82defbf2d5916243c1b5ef1f6b880f8b2d56ad7
erjones-UU <66476019+erjones-UU@users.noreply.github.com>
2022-07-21 10:39:56 +0200

    Add files via upload

--- ini/runDynQual.sh
+++ ini/runDynQual.sh
@@ -2,16 +2,16 @@
 #SBATCH -t 3:00:00
 #SBATCH -p thin
 #SBATCH -N 1
-#SBATCH -n 128
+#SBATCH -n 32
 
 cd /home/ejones/
 
 ###Online DynQual runs with W5E5 forcing (calcLoads == True or False)
-python DynQualModel/deterministic_runner.py ini/DynQual_05min.ini & #single landmask
+#python DynQualModel/deterministic_runner.py ini/DynQual_05min.ini & #single landmask
 #python DynQualModel/parallel_pcrglobwb_runner.py ini/DynQual_05min.ini & #parallel
 
 ###Offline DynQual runs with W5E5 forcing (calcLoads == False only.)
-python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini & #single landmask
+#python DynQualModel/deterministic_runner_offline.py ini/DynQual_05min_offline.ini & #single landmask
 #python DynQualModel/parallel_pcrglobwb_runner_offline.py ini/DynQual_05min_offline.ini & #parallel
 
-wait
+#wait
